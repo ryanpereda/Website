07ddad04e5cea05f228935e43f47319b6cf227bc
ryanpereda <ryanpereda9001@gmail.com>
2023-06-24 03:12:38 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    flash('Form submitted succesfully', 'success')
+
+    return redirect(url_for('contact'))
+
+app.run()
